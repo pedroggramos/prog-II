6bf799ebd8c008f3906087d0e14ab7ef5f92fd0e
Pedro Ramos <pedrogabrielguimaraesramos178@gmail.com>
2024-11-23 23:55:04 -0300

    up studies

--- Structs/studies/struct.c
+++ Structs/studies/struct.c
@@ -1,53 +1,60 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+// 1
 
-struct Aluno{
-    char nome[50];
-    float notas[3];
-    float media;
-};
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
 
-void media(struct Aluno aluno[], int qtd){
+// struct Aluno{
+//     char nome[50];
+//     float notas[3];
+//     float media;
+// };
 
-    for(int i = 0; i < qtd;i++){
-        float soma = 0;
+// void media(struct Aluno aluno[], int qtd){
+
+//     for(int i = 0; i < qtd;i++){
+//         float soma = 0;
     
-        for(int j = 0; j < 3;j++){
-            soma = soma + aluno[i].notas[j];
-        }
-        aluno[i].media = soma / 3;
-        printf("Média %d: %.2f\n", i + 1, aluno[i].media);
-    }
-}
+//         for(int j = 0; j < 3;j++){
+//             soma = soma + aluno[i].notas[j];
+//         }
+//         aluno[i].media = soma / 3;
+//         printf("Média %s: %.2f\n",aluno[i].nome, aluno[i].media);
+//     }
+// }
 
-int main(){
+// int main(){
 
-    int qtd;
+//     int qtd;
 
-    printf("Digite a qtd de Alunos: ");
-    scanf("%d", &qtd);
+//     printf("Digite a qtd de Alunos: ");
+//     scanf("%d", &qtd);
 
-    struct Aluno *aluno = (struct Aluno*) malloc(qtd * sizeof(struct Aluno));
+//     getchar();
 
-    for(int i = 0; i < qtd; i++){
-        printf("Digite o %d° nome: ", i + 1);
-        fgets(aluno[i].nome, sizeof(aluno[i].nome), stdin);
-        aluno[i].nome[strcspn(aluno[i].nome, "\n")] = '\0';
+//     struct Aluno *aluno = (struct Aluno*) malloc(qtd * sizeof(struct Aluno));
+
+//     for(int i = 0; i < qtd; i++){
+//         printf("Digite o %d° nome: ", i + 1);
+//         fgets(aluno[i].nome, sizeof(aluno[i].nome), stdin);
+//         aluno[i].nome[strcspn(aluno[i].nome, "\n")] = '\0';
         
-        while (getchar() != '\n');
+//         while (getchar() != '\n');
 
-        for(int j = 0; j < 3; j++){
-            printf("Digite a %d° nota do aluno %d: ", j + 1, i +  1);
-            scanf("%f", &aluno[i].notas[j]);
-        }
-    }
+//         for(int j = 0; j < 3; j++){
+//             printf("Digite a %d° nota do aluno %d: ", j + 1, i +  1);
+//             scanf("%f", &aluno[i].notas[j]);
+//         }
+        
+//         getchar();
+        
+//     }
 
-    media(aluno, qtd);
+//     media(aluno, qtd);
 
-    free(aluno);
+//     free(aluno);
 
     
 
-    return 0;
-}
+//     return 0;
+// }
